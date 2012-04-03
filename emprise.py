5363adecc4c0226b10db4ab49540f1de59297ae7
Laurent Bovet <laurent.bovet@windmaster.ch>
2012-04-03 23:28:23 +0200

    improved

--- emprise.py
+++ emprise.py
@@ -2,8 +2,38 @@ import logging
 logging.basicConfig()
 logging.root.setLevel(logging.INFO)
 
+class Receiver(object):    
+    def __init__(self):
+        import gtk
+        import keybinder
+        keystr = ""
+        keybinder.bind("F2", self.Q)
+        keybinder.bind("F3", self.W)
+        keybinder.bind("F4", self.A)
+        keybinder.bind("F5", self.S)
+        keybinder.bind("F7", self.Y)
+        keybinder.bind("F6", self.X)
+        keybinder.bind("F8", self.C)
+        gtk.main()
+    def Q(self):
+        Player.current_player.play_clicked()
+    def W(self):
+        Player.current_player.play_double_clicked()
+    def A(self):
+        Player.current_player.left_clicked()
+    def S(self):
+        Player.current_player.right_clicked()
+    def Y(self):
+        Player.current_player.down_clicked()
+    def X(self):
+        Player.current_player.up_clicked()
+    def C(self):
+        Player.current_player.up_down_clicked()
+
 class Player(object):
-    def _init_(self, name, default=False, dbus_name="<unknown>"):
+    current_player = None
+    default_player = None
+    def __init__(self, name, default=False, dbus_name="<unknown>"):
         self.name=name
         self.next_player=None
         self.default=default
@@ -13,32 +43,43 @@ class Player(object):
     def init(self):
         pass    
     def activate(self):
-        current_player=self
+        self.log.info("Activating %s", self.name)
+        Player.current_player=self
         if self.default:
-            default_player=self
-    def next_player(self):
-        return next_player
+            Player.default_player=self
     def toggle(self):
         self.log.info("Toggle")
     def stop(self):
-        self.log.info("
+        self.log.info("Stop")
+    def previous(self):
+        self.log.info("Previous")
+    def next(self):
+        self.log.info("Next")
+
     # Remote control
     def play_clicked(self):
         self.log.info( "Play clicked")
+        self.toggle()
     def play_double_clicked(self):
-        self.log.info( "Play double-clicked")
+        self.log.info( "Play double-clicked")        
     def left_clicked(self):
         self.log.info( "Left clicked")
+        self.previous()
     def right_clicked(self):
         self.log.info( "Right clicked")
+        self.next()
     def up_clicked(self):
-        self.log.info( "Up clicked")        
+        self.log.info( "Up clicked")   
+        self.stop()
+        self.next_player.activate()     
     def down_clicked(self):
-        self.log.info( "Down clicked")                  
+        self.log.info( "Down clicked") 
     def up_down_clicked(self):
         self.log.info( "Up/Down clicked")    
+        players['xbmc'].activate()
 
-class XMBC(Player):
+'''Special control for XBMC'''
+class XBMC(Player):
     def init(self):            
         import virtkey
         self.v = virtkey.virtkey()
@@ -56,20 +97,20 @@ class XMBC(Player):
     def up_clicked(self):
         self._click(0xFF52) # Up
     def down_clicked(self):
-        self._click(0xFF54)
+        self._click(0xFF54) # Down
     def up_down_clicked(self):
-        self._click(0x078) # STOP
-        self.next_player().activate()
-    def next_player():
-        return default_player
+        self._click(0x078) # X (STOP)
+        self.log.info("Back to %s", Player.default_player.name)
+        Player.default_player.activate()
 
 players = { 'banshee' : Player('banshee', default=True, dbus_name="org.mpris.banshee"),
-   'radio': Player('radio'), default=True, dbus_name="org.mpris.xmms2"),
-   'xbmc': Player('xbmc'), dbus_name="org.mpris.xbmc") }
+            'radio': Player('radio', default=True, dbus_name="org.mpris.xmms2"),
+            'xbmc': XBMC('xbmc', dbus_name="org.mpris.xbmc") }
 
 players['banshee'].next_player = players['radio']
 players['radio'].next_player = players['banshee']
 
-current_player = players['radio']
+Player.current_player = players['radio']
+Player.current_player.activate()
 
-current_player.activate()
+Receiver()
